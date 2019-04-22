61e39ccd7c5cc91846de47967891025f5203db0d
john Ng <ngkaipong1c24@gmail.com>
2019-04-22 23:40:18 +0800

    Add settings function

---
+++ app/src/main/java/com/vrem/wifianalyzer/settings/SettingsFragment.java
@@ -0,0 +1,40 @@
+/*
+ * WiFiAnalyzer
+ * Copyright (C) 2019  VREM Software Development <VREMSoftwareDevelopment@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.vrem.wifianalyzer.settings;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+import hkcc.ccn3165.assignment.crowdsource.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String rootKey) {
+        addPreferencesFromResource(R.xml.settings);
+        /* if (BuildUtils.isMinVersionP()) {
+            findPreference(getString(R.string.scan_interval_key)).setVisible(false);
+            findPreference(getString(R.string.scan_fast_key)).setVisible(true);
+        } else {
+            findPreference(getString(R.string.scan_interval_key)).setVisible(true);
+            findPreference(getString(R.string.scan_fast_key)).setVisible(false);
+        } */
+    }
+
+}
